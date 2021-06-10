9f689b4ed3bfc54718da7dc9063d7326d7e6d86f
andrealemonroy <andreale17@icloud.com>
2021-06-09 20:22:01 -0500

    fix deploy

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -9,7 +9,7 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
+// import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
