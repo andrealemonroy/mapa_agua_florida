44838075f33942bebe66b20674fc7bc656ddbef9
fiorellasaro <fiorellasaro27@gmail.com>
2022-02-07 19:55:27 -0500

    styling map, cards

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -7,11 +7,17 @@ const Wrapper = styled.main`
   display: flex;
   width: 100vw;
   height: calc(100vh - 64px);
+  @media (max-width: 600px) {
+    flex-direction: column
+  }
 `;
 const ContainerMap = styled.div`
   display: flex;
-  width: 800px;
+  width: 50%;
   height: calc(100vh - 64px);
+  @media (max-width: 600px) {
+    width: 100vw
+  }
 `;
 const DotMarker = styled.div`
   width: 25px;
@@ -35,13 +41,16 @@ const DotMarkerBands = styled.div`
   }
 `;
 const ContainerCard = styled.div`
-  width: calc(100vw - 800px);
+  width: 50%;
   height: 100%;
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
   margin: 10px;
   overflow: scroll;
+  @media (max-width: 600px) {
+    width: 100%
+  }
 `;
 
 export const Map = () => {
@@ -49,6 +58,7 @@ export const Map = () => {
   const [bandsLocation, setBandsLocation] = useState([]);
   const [users, setUsers] = useState([]);
   const [bands, setBands] = useState([]);
+  const [selected, setSelected] = useState(false);
   axios.defaults.baseURL = `${process.env.API_AGUA_FLORIDA}`;
   const instance = axios.create({
     baseURL: "https://api.aguaflorida.pe/api",
@@ -73,8 +83,8 @@ export const Map = () => {
     <Wrapper>
       <ContainerMap>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyCPzQz0K77TBg7KX8mtBis5Q4sVE3JD2I8" }}
-          defaultZoom={5.5}
+          // bootstrapURLKeys={{ key: "AIzaSyCPzQz0K77TBg7KX8mtBis5Q4sVE3JD2I8" }}
+          defaultZoom={5.2}
           defaultCenter={[-10.046374, -77.042793]}
         >
           {usersLocation ? (
@@ -111,48 +121,61 @@ export const Map = () => {
         {users.length > 0 ? (
           users.map((user) => {
             return (
-              <div class="bg-white py-8 px-10 text-center rounded-md shadow-lg transform max-w-xs mx-auto my-2 border-2 w-72">
-                <h2 class="font-semibold text-2xl mb-6">{user.fullName}</h2>
+              <div
+                key={user._id}
+                className="bg-white py-8 px-10 text-center rounded-md shadow-lg transform mx-auto my-2 border-2 md:w-64 sm:w-full"
+              >
+                <h2 className="font-semibold text-2xl mb-6">{user.fullName}</h2>
                 <img
-                  class="w-20 h-20 object-cover rounded-full mx-auto shadow-lg"
+                  className="w-20 h-20 object-cover rounded-full mx-auto shadow-lg"
                   src={user.photo ? user.photo : SINFOTO}
                   alt="User avatar"
                 />
-                <p class="capitalize text-xl mt-1">{user.genres}</p>
-                <span class="flex items-center border rounded-full w-24 pr-2 justify-center mx-auto mt-2 mb-12">
-                  <div class="bg-green-400 rounded-full w-2.5 h-2.5 block mr-2"></div>
+                <p className="capitalize text-xl mt-1">{user.genres}</p>
+                <span className="flex items-center border rounded-full w-24 pr-2 justify-center mx-auto mt-2 mb-12">
+                  <div className="bg-green-400 rounded-full w-2.5 h-2.5 block mr-2"></div>
                   Nombre
                 </span>
-                <button class="rounded-md bg-gradient-to-r from-blue-400 to-indigo-500 text-xl text-white pt-3 pb-4 px-8 inline">
+                <button className="rounded-md bg-gradient-to-r from-blue-400 to-indigo-500 text-xl text-white pt-3 pb-4 px-8 inline">
                   Redes
                 </button>
               </div>
             );
           })
+        ) : selected ? (
+          <span>No Hay Personas/Solistas en esa localidad</span>
         ) : (
           <span></span>
         )}
+
         {bands.length > 0 ? (
           bands.map((band) => {
             return (
-              <div class="bg-white py-8 px-10 text-center rounded-md shadow-lg transform  max-w-xs mx-auto my-2 border-2 w-72">
-                <h2 class="font-semibold text-2xl mb-6">{band.bandsName}</h2>
+              <div
+                key={band._id}
+                className="bg-white py-8 px-10 text-center rounded-md shadow-lg transform mx-auto my-2 border-2 w-64"
+              >
+                <h2 className="font-semibold text-2xl mb-6">
+                  {band.bandsName}
+                </h2>
                 <img
-                  class="w-20 h-20 object-cover rounded-full mx-auto shadow-lg"
+                  className="w-20 h-20 object-cover rounded-full mx-auto shadow-lg"
                   src={band.photo ? band.photo : SINFOTO}
                   alt="User avatar"
                 />
-                <p class="capitalize text-xl mt-1">{band.genres}</p>
-                <span class="flex items-center border rounded-full w-24 pr-2 justify-center mx-auto mt-2 mb-12">
-                  <div class="bg-green-400 rounded-full w-2.5 h-2.5 block mr-2"></div>
+                <p className="capitalize text-xl mt-1">{band.genres}</p>
+                <span className="flex items-center border rounded-full w-24 pr-2 justify-center mx-auto mt-2 mb-12">
+                  <div className="bg-green-400 rounded-full w-2.5 h-2.5 block mr-2"></div>
                   Nombre
                 </span>
-                <button class="rounded-md bg-gradient-to-r from-blue-400 to-indigo-500 text-xl text-white pt-3 pb-4 px-8 inline">
+                <button className="rounded-md bg-gradient-to-r from-blue-400 to-indigo-500 text-xl text-white pt-3 pb-4 px-8 inline">
                   Redes
                 </button>
               </div>
             );
           })
+        ) : selected ? (
+          <span>No Hay Bandas en esa localidad</span>
         ) : (
           <span></span>
         )}
