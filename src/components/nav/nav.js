7fe75711ba247b7db94c7c90b8f306facdc123f7
andrealemonroy <andreale17@icloud.com>
2022-02-20 21:43:10 -0500

    nav mobile

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -31,17 +31,7 @@ export default function Nav(props) {
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
+
               <div className={classNames(
                     props.route === "personalForm" || props.route === "bandForm"
                       ? "justify-center"
@@ -51,7 +41,7 @@ export default function Nav(props) {
                 <div className="flex-shrink-0 flex items-center">
                   <Link to="/">
                     <img
-                      className="block lg:hidden h-8 w-auto"
+                      className="block lg:hidden h-6 w-auto"
                       src={logo}
                       alt="Workflow"
                     />
@@ -78,6 +68,17 @@ export default function Nav(props) {
                     ))}
                   </div>
                 </div>
+              </div>
+              <div className=" inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Abrir menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
               </div>
             </div>
           </div>
